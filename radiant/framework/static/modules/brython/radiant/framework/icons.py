86ed13aa37a556c6fa1b670a2d752c9e3fbf0341
Yeison Cardona <yencardonaal@unal.edu.co>
2024-01-14 22:15:09 -0500

    Added **kwargs and default `mode` argument to Font-Awesome manager.

--- radiant/framework/static/modules/brython/radiant/framework/icons.py
+++ radiant/framework/static/modules/brython/radiant/framework/icons.py
@@ -1,24 +1,28 @@
 from browser import html
 
+
 # ----------------------------------------------------------------------
-def fa(icon):
+def fa(icon, mode='solid', *args, **kwargs):
     """"""
     if icon.startswith('fa-'):
         icon = icon[3:]
-    return html.I(Class=f'fa-solid fa-{icon}')
+    extra_class = kwargs.pop('Class', '')
+    return html.I(Class=f'fa-{mode} fa-{icon} {extra_class}', *args, **kwargs)
 
 
 # ----------------------------------------------------------------------
-def bi(icon):
+def bi(icon, *args, **kwargs):
     """"""
     if icon.startswith('bi-'):
         icon = icon[3:]
-    return html.I(Class=f'bi bi-{icon}')
+    extra_class = kwargs.pop('Class', '')
+    return html.I(Class=f'bi bi-{icon} {extra_class}', *args, **kwargs)
 
 
 # ----------------------------------------------------------------------
-def mi(icon, size=48):
+def mi(icon, size=48, *args, **kwargs):
     """"""
     if icon.startswith('md-'):
         icon = icon[3:]
-    return html.SPAN(icon, Class=f'material-icons md-{size}')
+    extra_class = kwargs.pop('Class', '')
+    return html.SPAN(icon, Class=f'material-icons md-{size} {extra_class}', *args, **kwargs)
