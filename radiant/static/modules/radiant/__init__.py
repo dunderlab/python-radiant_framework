bec20a9aab6f30af036d30467a4794f4ff902137
Yeison <yencardonaal@unal.edu.co>
2026-01-31 22:06:15 -0500

    Add HTTP utilities (GET, POST, and VIEW) with decorators
    Implemented `get`, `post`, and `view` class methods to handle HTTP requests using fetch API and JSON parsing. Added URL query handling for GET and request body handling for POST. This enhances the module's ability to interact with APIs effectively.

--- radiant/static/modules/radiant/__init__.py
+++ radiant/static/modules/radiant/__init__.py
@@ -1,7 +1,12 @@
-from typing import Any
+from typing import Any, Callable
 
 from radiant.core import Element, html, select
 from fake import Fake
+
+from browser import ajax
+from urllib.parse import urlencode
+from browser import window
+import json
 
 from functools import wraps
 
@@ -34,10 +39,63 @@ class BythonServer:
         """
         return Element(element)
 
-    def get(cls, *args, **kwargs):
+    @classmethod
+    def get(cls, route) -> Callable:
         """"""
-        return Fake
+
+        def decorator(fn):
+            @wraps(fn)
+            def wrapper(cls, **kwargs):
+                if kwargs:
+                    query = urlencode(kwargs, doseq=True)
+                    url = f"{route}?{query}"
+                else:
+                    url = route
+                return (
+                    window.fetch(url)
+                    .then(lambda r: r.text())
+                    .then(lambda text: json.loads(text))
+                )
+
+            return wrapper
+
+        return decorator
+
+    @classmethod
+    def post(cls, route):
+
+        def decorator(fn):
+            @wraps(fn)
+            def wrapper(cls, **kwargs):
+                body = json.dumps(kwargs)
+
+                return (
+                    window.fetch(
+                        route,
+                        {
+                            "method": "POST",
+                            "headers": {"Content-Type": "application/json"},
+                            "body": body,
+                        },
+                    )
+                    .then(lambda r: r.text())
+                    .then(lambda text: json.loads(text))
+                )
+
+            return wrapper
+
+        return decorator
+
+    @classmethod
+    def view(cls, route) -> Callable:
+        def decorator(fn):
+            @wraps(fn)
+            def wrapper(*args, **kwargs):
+                return fn(*args, **kwargs)
+
+            return wrapper
+
+        return decorator
 
 
 json_response = Fake()
-# html_response = Fake()
